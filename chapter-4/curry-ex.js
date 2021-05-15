902b4d32b2cf19c09e908f2b4ed7e6e7c2c10d82
jb9229 <jb9229@gmail.com>
2021-05-15 11:46:19 +0900

    feature: initialize exercise functional programming

---
+++ chapter-4/curry-ex.js
@@ -0,0 +1,22 @@
+// import * as R from "ramda";
+const R = require("ramda");
+
+// states
+const useDb = true;
+const db = {};
+const arr = ["111-11-1111", "222-22-2222", "333-33-3333"];
+const find = (db, ssn) => {
+  return {};
+};
+
+const fetchStudentFromDb = R.curry(function (db, ssn) {
+  return find(db, ssn);
+});
+
+const fetchStudentFromArray = R.curry(function (arr, ssn) {
+  return arr[ssn];
+});
+
+const findStudent = useDb ? fetchStudentFromDb(db) : fetchStudentFromArray(arr);
+
+console.log(findStudent("333-33-3333"));
